96588afc3b101a85b32fbddbadb2b0511fca51fb
Mat Wong <matwong05@gmail.com>
2021-12-06 20:02:13 -0500

    first commit

--- model.py
+++ model.py
@@ -10,13 +10,18 @@ def init_params(m):
     classname = m.__class__.__name__
     if classname.find("Linear") != -1:
         m.weight.data.normal_(0, 1)
-        m.weight.data *= 1 / torch.sqrt(m.weight.data.pow(2).sum(1, keepdim=True))
+        m.weight.data *= 1 / torch.sqrt(
+            m.weight.data.pow(2).sum(1, keepdim=True))
         if m.bias is not None:
             m.bias.data.fill_(0)
 
 
 class ACModel(nn.Module, torch_ac.RecurrentACModel):
-    def __init__(self, obs_space, action_space, use_memory=False, use_text=False):
+    def __init__(self,
+                 obs_space,
+                 action_space,
+                 use_memory=False,
+                 use_text=False):
         super().__init__()
 
         # Decide which components are enabled
@@ -24,29 +29,29 @@ class ACModel(nn.Module, torch_ac.RecurrentACModel):
         self.use_memory = use_memory
 
         # Define image embedding
-        self.image_conv = nn.Sequential(
-            nn.Conv2d(3, 16, (2, 2)),
-            nn.ReLU(),
-            nn.MaxPool2d((2, 2)),
-            nn.Conv2d(16, 32, (2, 2)),
-            nn.ReLU(),
-            nn.Conv2d(32, 64, (2, 2)),
-            nn.ReLU()
-        )
+        self.image_conv = nn.Sequential(nn.Conv2d(3, 16, (2, 2)), nn.ReLU(),
+                                        nn.MaxPool2d((2, 2)),
+                                        nn.Conv2d(16, 32, (2, 2)), nn.ReLU(),
+                                        nn.Conv2d(32, 64, (2, 2)), nn.ReLU())
         n = obs_space["image"][0]
         m = obs_space["image"][1]
-        self.image_embedding_size = ((n-1)//2-2)*((m-1)//2-2)*64
+        self.image_embedding_size = ((n - 1) // 2 - 2) * (
+            (m - 1) // 2 - 2) * 64
 
         # Define memory
         if self.use_memory:
-            self.memory_rnn = nn.LSTMCell(self.image_embedding_size, self.semi_memory_size)
+            self.memory_rnn = nn.LSTMCell(self.image_embedding_size,
+                                          self.semi_memory_size)
 
         # Define text embedding
         if self.use_text:
             self.word_embedding_size = 32
-            self.word_embedding = nn.Embedding(obs_space["text"], self.word_embedding_size)
+            self.word_embedding = nn.Embedding(obs_space["text"],
+                                               self.word_embedding_size)
             self.text_embedding_size = 128
-            self.text_rnn = nn.GRU(self.word_embedding_size, self.text_embedding_size, batch_first=True)
+            self.text_rnn = nn.GRU(self.word_embedding_size,
+                                   self.text_embedding_size,
+                                   batch_first=True)
 
         # Resize image embedding
         self.embedding_size = self.semi_memory_size
@@ -54,25 +59,19 @@ class ACModel(nn.Module, torch_ac.RecurrentACModel):
             self.embedding_size += self.text_embedding_size
 
         # Define actor's model
-        self.actor = nn.Sequential(
-            nn.Linear(self.embedding_size, 64),
-            nn.Tanh(),
-            nn.Linear(64, action_space.n)
-        )
+        self.actor = nn.Sequential(nn.Linear(self.embedding_size, 64),
+                                   nn.Tanh(), nn.Linear(64, action_space.n))
 
         # Define critic's model
-        self.critic = nn.Sequential(
-            nn.Linear(self.embedding_size, 64),
-            nn.Tanh(),
-            nn.Linear(64, 1)
-        )
+        self.critic = nn.Sequential(nn.Linear(self.embedding_size, 64),
+                                    nn.Tanh(), nn.Linear(64, 1))
 
         # Initialize parameters correctly
         self.apply(init_params)
 
     @property
     def memory_size(self):
-        return 2*self.semi_memory_size
+        return 2 * self.semi_memory_size
 
     @property
     def semi_memory_size(self):
@@ -84,7 +83,8 @@ class ACModel(nn.Module, torch_ac.RecurrentACModel):
         x = x.reshape(x.shape[0], -1)
 
         if self.use_memory:
-            hidden = (memory[:, :self.semi_memory_size], memory[:, self.semi_memory_size:])
+            hidden = (memory[:, :self.semi_memory_size],
+                      memory[:, self.semi_memory_size:])
             hidden = self.memory_rnn(x, hidden)
             embedding = hidden[0]
             memory = torch.cat(hidden, dim=1)
